6991b385e5919add230240ad52f0bcade8b14ec4
Aaron Sheah <aaronsheah@gmail.com>
2021-12-15 23:39:18 +0000

    simple line chart rendering using go-echarts

--- main.go
+++ main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"strconv"
 
 	"CFDGolang/pkg/linearconvection"
+
+	"github.com/go-echarts/go-echarts/v2/charts"
+	"github.com/go-echarts/go-echarts/v2/opts"
+	"github.com/go-echarts/go-echarts/v2/types"
 )
 
 func setupInitialVelocities(c *linearconvection.OneDimensionLinearConvectionConfig) []int {
@@ -23,6 +29,33 @@ func setupInitialVelocities(c *linearconvection.OneDimensionLinearConvectionConf
 	return velocities
 }
 
+func httpServer(
+	lineDatum []opts.LineData,
+	xAxisLabels []string,
+) func(w http.ResponseWriter, _ *http.Request) {
+	return func(w http.ResponseWriter, _ *http.Request) {
+		line := charts.NewLine()
+
+		line.SetGlobalOptions(
+			charts.WithInitializationOpts(opts.Initialization{Theme: types.ThemeInfographic}),
+			charts.WithTitleOpts(opts.Title{
+				Title:    "Line example in Westeros theme",
+				Subtitle: "Line chart rendered by the http server this time",
+			}))
+
+		line.SetXAxis(xAxisLabels)
+		line.AddSeries("Test", lineDatum)
+		line.SetSeriesOptions(
+			charts.WithLineChartOpts((opts.LineChart{
+				Smooth: true,
+				Step:   true,
+			})),
+		)
+
+		line.Render(w)
+	}
+}
+
 func main() {
 	gridPoints, timesteps, timeUnit, wavespeed := 41, 25, float64(0.025), 1
 	config := linearconvection.NewOneDimensionLinearConvectionConfig(gridPoints, timesteps, timeUnit, wavespeed)
@@ -30,4 +63,16 @@ func main() {
 	velocities := setupInitialVelocities(config)
 
 	fmt.Println(velocities)
+
+	points := make([]opts.LineData, len(velocities))
+	xAxisLabels := make([]string, len(velocities))
+	for i := 0; i < len(velocities); i++ {
+		points[i] = opts.LineData{
+			Value: velocities[i],
+		}
+		xAxisLabels[i] = strconv.Itoa(i)
+	}
+
+	http.HandleFunc("/", httpServer(points, xAxisLabels))
+	http.ListenAndServe(":1234", nil)
 }
